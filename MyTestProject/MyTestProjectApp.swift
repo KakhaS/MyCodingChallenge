ddaf2ff2f78e4107a80afc1f779e05607a0ab5d2
Kakha <kakha@Kakhas-MacBook-Air.local>
2025-08-15 19:19:10 +0400

    Initial XCode Project setup

---
+++ MyTestProject/MyTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTestProjectApp.swift
+//  MyTestProject
+//
+//  Created by Kakha on 15.08.25.
+//
+
+import SwiftUI
+
+@main
+struct MyTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyTestProjectTests/MyTestProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyTestProjectTests.swift
+//  MyTestProjectTests
+//
+//  Created by Kakha on 15.08.25.
+//
+
+import Testing
+@testable import MyTestProject
+
+struct MyTestProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
