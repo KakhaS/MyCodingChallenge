3e7a164b3a8e80456757e0acb1e995ce7b2bbc61
Kakha <kakha@Kakhas-MacBook-Air.local>
2025-08-18 22:40:14 +0400

    Added Async await URL call

--- MyTestProject/ContentView.swift
+++ MyTestProject/ContentView.swift
@@ -52,9 +52,12 @@ struct ContentView: View {
                     .padding(30)
                 Button {
                     if !cityName.isEmpty {
-                        fetchWeather(for: cityName) { info, type  in
-                            weatherInfo = info
-                            currentWeatherType = type
+                        Task {
+                            let (info, type) = await fetchWeatherAsync(for: cityName)
+                            await MainActor.run {
+                                weatherInfo = info
+                                currentWeatherType = type
+                            }
                         }
                     }
                 } label: {
@@ -88,41 +91,60 @@ struct ContentView: View {
             .padding()
         }
     }
-    func fetchWeather(for city: String, completion: @escaping (String, WeatherType) -> Void) {
+//    func fetchWeather(for city: String, completion: @escaping (String, WeatherType) -> Void) {
+//        let cityQuery = city.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? city
+//        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(cityQuery)&appid=69aadee7d4e41d76919edfe23d9dd979&units=metric"
+//        
+//        guard let url = URL(string: urlString) else {
+//            completion("URL is not correct", .unknown)
+//            return
+//        }
+//        URLSession.shared.dataTask(with: url) { data, _ , error in
+//            if let error = error {
+//                DispatchQueue.main.async {
+//                    completion("Error: \(error.localizedDescription)", .unknown)
+//                }
+//                return
+//            }
+//            guard let data = data else {
+//                DispatchQueue.main.async {
+//                    completion("NoData", .unknown)
+//                }
+//                return
+//            }
+//            do {
+//                let decoded = try JSONDecoder().decode(WeatherResponse.self, from: data)
+//                let info = "\(decoded.name): \(Int(decoded.main.temp))°C, \(decoded.weather.first?.description ?? "")"
+//                let type = WeatherType(rawValue: decoded.weather.first?.main ?? "Unknown") ?? .unknown
+//                completion(info, type)
+//                
+//            }
+//            catch {
+//                DispatchQueue.main.async {
+//                    completion("City name is not correct", .unknown)
+//                }
+//            }
+//            
+//        }.resume()
+//    }
+    func fetchWeatherAsync(for city: String) async -> (String, WeatherType) {
         let cityQuery = city.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? city
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(cityQuery)&appid=69aadee7d4e41d76919edfe23d9dd979&units=metric"
+            let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(cityQuery)&appid=69aadee7d4e41d76919edfe23d9dd979&units=metric"
         
         guard let url = URL(string: urlString) else {
-            completion("URL is not correct", .unknown)
-            return
+            return ("URL is not correct", .unknown)
         }
-        URLSession.shared.dataTask(with: url) { data, _ , error in
-            if let error = error {
-                DispatchQueue.main.async {
-                    completion("Error: \(error.localizedDescription)", .unknown)
-                }
-                return
-            }
-            guard let data = data else {
-                DispatchQueue.main.async {
-                    completion("NoData", .unknown)
-                }
-                return
-            }
-            do {
-                let decoded = try JSONDecoder().decode(WeatherResponse.self, from: data)
-                let info = "\(decoded.name): \(Int(decoded.main.temp))°C, \(decoded.weather.first?.description ?? "")"
-                let type = WeatherType(rawValue: decoded.weather.first?.main ?? "Unknown") ?? .unknown
-                completion(info, type)
-                
-            }
-            catch {
-                DispatchQueue.main.async {
-                    completion("Failed To decode JSON", .unknown)
-                }
-            }
-            
-        }.resume()
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decoded = try JSONDecoder().decode(WeatherResponse.self, from: data)
+            let info = "\(decoded.name): \(Int(decoded.main.temp))°C, \(decoded.weather.first?.description ?? "")"
+            let type = WeatherType(rawValue: decoded.weather.first?.main ?? "") ?? .unknown
+            return (info, type)
+        } catch {
+            return ("Not Correct City Name", .unknown)
+        }
+        
     }
 }
 
